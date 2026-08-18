27d11a9daa0456f3af53f8511b779473e08f94d0
christeena2006 <teenaleana@gmail.com>
2026-08-18 10:55:00 +0530

    updated shared backend URLs for login and registration

--- src/pages/CustomerLogin.jsx
+++ src/pages/CustomerLogin.jsx
@@ -17,7 +17,7 @@ function CustomerLogin() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("http://10.188.62.112:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
