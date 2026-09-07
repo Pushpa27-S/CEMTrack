7ccb3ecf8733b5dd698a8918d428f67a2611e4ef
christeena2006 <teenaleana@gmail.com>
2026-09-07 20:26:14 +0530

    updated cartRoutes.js

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,16 +1,19 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+  path: new URL("./.env", import.meta.url)
+});
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
 });
 
 export default db;
